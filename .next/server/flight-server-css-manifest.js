17e99518dc0be0f1a345108bf5075aa2e6ff720b
Yash Shah <shayash41@gmail.com>
2023-08-23 16:34:57 -0400

    extracting the folder

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/yashshah/Desktop/yashshah41.github.io/app/page": [
+      "/Users/yashshah/Desktop/yashshah41.github.io/styles/globals.css"
+    ]
+  },
+  "/Users/yashshah/Desktop/yashshah41.github.io/app/layout.tsx": [
+    "/Users/yashshah/Desktop/yashshah41.github.io/styles/globals.css"
+  ]
+}
