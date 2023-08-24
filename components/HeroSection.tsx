7bca0a70b247bd1d63b94899e049a2df9c6e5b90
Yash Shah <shayash41@gmail.com>
2023-08-24 13:14:58 -0400

    s

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,11 +5,7 @@ import { Link } from "react-scroll/modules"
 import { HiArrowDown } from "react-icons/hi"
 
 const HeroSection = () => {
-  const setActiveTab = (e) => {
-    e.preventDefault();
-    console.log(e.target);
-  }
-  
+
   return (
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
